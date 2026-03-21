c55e6619d1168b742cf0094324f196320d38b700
bollig <bollig@lmf.cnrs.fr>
2026-03-21 23:32:29 +0100

    Update diagnosis example

--- examples/diagnosis.py
+++ examples/diagnosis.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     USE_UI = True
 
     diagnosisConsensus.configure(
-        llms={"LLM1": "mistral", "LLM2": "openai"},
+        llms="mock",
         ui=USE_UI,
         timeout=600,
     )
